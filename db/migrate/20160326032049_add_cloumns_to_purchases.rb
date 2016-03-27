13be95196af324255a2aaf3b0d63dd13c9d4c74f
linan <linan.avvo@gmail.com>
2016-03-27 10:41:39 +0800

    修改生产任务

--- db/migrate/20160326032049_add_cloumns_to_purchases.rb
+++ db/migrate/20160326032049_add_cloumns_to_purchases.rb
@@ -1,7 +1,7 @@
 class AddCloumnsToPurchases < ActiveRecord::Migration
   def change
     add_column :purchases, :checker, :string #审核人
-    add_column :purchases, :check_status, :integer #审核状态
+    add_column :purchases, :check_status, :integer, default: 0 #审核状态
     add_column :purchases, :check_date, :datetime # 审核日期
     add_reference :purchases, :supplier, index: true, foreign_key: true #供应商
   end
