bdce2ad845f34986ff2d411c575aa962d72d7409
linan <linan.avvo@gmail.com>
2016-04-12 20:28:34 +0800

    downlaod

--- app/models/order_unit.rb
+++ app/models/order_unit.rb
@@ -2,6 +2,8 @@ class OrderUnit < ActiveRecord::Base
   has_one :produce_tasks, as: :item
   belongs_to :order
 
+  attr_accessor :ply, :texture, :face, :color
+
   enum state: {
     not_create: 0,       # 未创建生产任务
     created: 1,    # 已创建生产任务
